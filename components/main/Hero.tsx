903510c99cb32089661de0906cad047b8ef4f336
Jnanesh <jnaneshps4@gmail.com>
2023-12-08 07:23:11 +0530

    14th commit

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import HeroContent from "../sub/HeroContent";
 
 const Hero = () => {
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -62,7 +62,7 @@ const HeroContent = () => {
                   </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
-          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+          className="py-2 button-primary text-center text-white rounded-lg max-w-[200px]"
         >
           Learn More!
         </motion.a>
